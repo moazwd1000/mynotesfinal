258088a873959a99a88959f3c95aaa93f1dce451
moazwd1000 <moaz.walead1@gmail.com>
2023-04-08 00:11:28 +0200

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotesfinal/views/login_view.dart';
 import 'package:mynotesfinal/views/registor_view.dart';
+import 'package:mynotesfinal/views/verify_email.dart';
 
 import 'firebase_options.dart';
 
@@ -33,14 +34,18 @@ class HomePage extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         switch (snapshot.connectionState) {
           case (ConnectionState.done):
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user!.emailVerified) {
-            // } else {
-            //   print("not verified");
-            //   return const EmailVerifyView();
-            // }
-            // return Text("Done");
-            return LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is Verified");
+              } else {
+                return EmailVerifyView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
+
           default:
             // ignore: prefer_const_constructors
             return CircularProgressIndicator();
